d0c6cf5100bd5c28453546c058854f66459dd75b
matheuszbb <57190699+matheuszbb@users.noreply.github.com>
2021-03-10 13:38:32 -0300

    Delete NFe Analizer WEB directory

--- NFe Analizer WEB/modulo_limpeza.py
+++
@@ -1,5 +0,0 @@
-def limpeza(deleta_xml,os):
-    for x in deleta_xml:
-        for xml in os.listdir(os.path.expanduser('~')+"/Desktop/Aprovada"):
-            if (xml == x):
-              os.remove(os.path.expanduser('~')+"/Desktop/Aprovada/" + xml)
